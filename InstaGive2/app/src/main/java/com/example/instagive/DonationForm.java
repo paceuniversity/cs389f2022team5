00b0b0409b0288c30ea122f878b03b8dd15575cc
leanna <leamachado1204@gmail.com>
2022-11-07 14:44:50 -0500

    Created Donation Form and Summary Activities

---
+++ InstaGive2/app/src/main/java/com/example/instagive/DonationForm.java
@@ -0,0 +1,14 @@
+package com.example.instagive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DonationForm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_donation_form);
+    }
+}
