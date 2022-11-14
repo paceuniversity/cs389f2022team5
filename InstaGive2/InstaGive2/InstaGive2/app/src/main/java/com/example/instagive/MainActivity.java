4039a85862b0af7ff9ef522c3d33c93c661631c4
CJ <cl32484p@pace.edu>
2022-11-14 01:30:51 -0500

    Changed Start Screen Colors

---
+++ InstaGive2/InstaGive2/InstaGive2/app/src/main/java/com/example/instagive/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.instagive;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.instagive.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+    private static final String LOG_TAG = DonationForm.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
+    }
+
+    public void launchDonationForm(View view) {
+        Log.d(LOG_TAG, "Donate button clicked!"); //Just for the Log
+
+        Intent intent = new Intent(this,DonationForm.class);
+        startActivity(intent);
+    }
+
+}
